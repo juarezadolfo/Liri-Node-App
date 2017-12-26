b8094f61c35f4e7ca6eb94ffa27ba29402e3e67d
juarezadolfo <juarezadolfo@hotmail.com>
2017-12-25 23:56:19 -0800

    added OMDB Request - broken but almost there

--- liri.js
+++ liri.js
@@ -15,49 +15,84 @@ var spotify = new Spotify(keys.spotifyKeys);
 // 	//SPOTIFY
 // eventually add spotify function
 // function spotifyThisSong(songName) {
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-      if (err) {
+spotify.search({ type: 'track', query: 'The Sign' }, function (err, data) {
+    if (err) {
         return console.log('Error occurred: ' + err);
-      }
-      // loop through every item
-      for (var i = 0; i < 5; i++) {
+    }
+
+    for (var i = 0; i < 5; i++) {
         console.log("------------------------------------");
-        // loop through the artists array
+
         for (var j = 0; j < data.tracks.items[i].artists.length; j++) {
-          console.log("Artist(s): " + data.tracks.items[i].artists[j].name);
+            console.log("Artist(s): " + data.tracks.items[i].artists[j].name);
         }
-        // print the information
+
         console.log("Song's Name: " + data.tracks.items[i].name);
         console.log("Preview Link: " + data.tracks.items[i].preview_url);
         console.log("Album: " + data.tracks.items[i].album.name);
-      }
-  
-    });
+    }
+
+});
 //   };
-
-
-
 
 // 	//TWITTER
 // //if user input is 'my-tweets' 
 // this will post the text only from my last 20 tweets
 // eventually add Twitter function
-    // function myTweets() {
-        var params = {screen_name: 'juarezadolfo'};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+// function myTweets() {
+var params = { screen_name: 'juarezadolfo' };
+client.get('statuses/user_timeline', params, function (error, tweets, response) {
     if (!error) {
-        for (var i = 0; i < 20; i++){
+        for (var i = 0; i < 20; i++) {
             console.log("Adolfo Tweet #" + i + ": " + tweets[i].text);
         }
     }
-  });
+});
 // };
 
+// OMDB Request
+
+// Store all of the arguments in an array
+var nodeArgs = process.argv;
+
+// Create an empty variable for holding the movie name
+var movieName = "";
+
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+for (var i = 2; i < nodeArgs.length; i++) {
+
+    if (i > 2 && i < nodeArgs.length) {
+
+        movieName = movieName + "+" + nodeArgs[i];
+
+    }
+
+    else {
+
+        movieName += nodeArgs[i];
+
+    }
+}
+
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+request(queryUrl, function (error, response, body) {
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+
+        // Parse the body of the site and recover just the imdbRating
+        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+        console.log("Release Year: " + JSON.parse(body).Year);
+    }
+});
 
 
-
-
-  
 
 
 
