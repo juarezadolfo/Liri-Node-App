c772a6d50c1d9e41ca2fe7af43797c9ab1f58e74
juarezadolfo <juarezadolfo@hotmail.com>
2017-12-25 21:01:22 -0800

    twitter works great, returns only the text now

--- liri.js
+++ liri.js
@@ -4,24 +4,21 @@ var input = process.argv[2];
 //All requires for NPM packages.
 var keys = require('./key.js');
 var fs = require('fs');
-var Twitter = require('twitter')
+var Twitter = require('twitter');
 // var spotify = require('spotify');
 var request = require('request');
 
 // 	//TWITTER
 // //if user input is my-tweets this will post the text only from my last 20 tweets
 var client = new Twitter(keys.twitterKeys);
-// var client = new Twitter({
-//     consumer_key: 'KiPAGfYoR0fzW05CXj0pP8dYV',
-//     consumer_secret: 'RAXI0f4IP4oc2Kk9x928pYAca7BGKOCbvOqjlttzGfOqiBKh4U',
-//     access_token_key: '250213554-WEUCBJ5aPORDCUA3gQIkmcjpaJPQDoBKsiasuGl5',
-//     access_token_secret: 'sDMz1YERulLnmQWYs094H51ctdZA65SqUW2UOpjYIWuut'
-//   });
+
    
   var params = {screen_name: 'juarezadolfo'};
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
-      console.log(tweets);
+        for (var i = 0; i < 20; i++){
+            console.log(tweets[i].text);
+        }
     }
   });
 
