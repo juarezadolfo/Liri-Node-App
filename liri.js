56bd0d537ca7608b417ba0e12c2ee1e8d5389927
juarezadolfo <juarezadolfo@hotmail.com>
2017-12-26 15:39:26 -0800

    added 'do what it says' fs read & spotify - broken but haven't defined functions or switch case yet

--- liri.js
+++ liri.js
@@ -1,5 +1,9 @@
+// Take in the command line arguments
+var nodeArgs = process.argv;
 //captures the user input on the command line
-// var input = process.argv[2];
+var input = process.argv[2];
+// Create an empty string for holding the command line 'input'
+var x = "";
 
 //All requires for NPM packages.
 var keys = require('./key.js');
@@ -12,43 +16,48 @@ var request = require('request');
 var client = new Twitter(keys.twitterKeys);
 var spotify = new Spotify(keys.spotifyKeys);
 
-// 	//SPOTIFY
-// eventually add spotify function
-// function spotifyThisSong(songName) {
-spotify.search({ type: 'track', query: 'The Sign' }, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
 
-    for (var i = 0; i < 5; i++) {
-        console.log("------------------------------------");
 
-        for (var j = 0; j < data.tracks.items[i].artists.length; j++) {
-            console.log("Artist(s): " + data.tracks.items[i].artists[j].name);
-        }
-
-        console.log("Song's Name: " + data.tracks.items[i].name);
-        console.log("Preview Link: " + data.tracks.items[i].preview_url);
-        console.log("Album: " + data.tracks.items[i].album.name);
-    }
-
-});
-//   };
 
 // 	//TWITTER
 // //if user input is 'my-tweets' 
 // this will post the text only from my last 20 tweets
 // eventually add Twitter function
 // function myTweets() {
-var params = { screen_name: 'juarezadolfo' };
-client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (!error) {
-        for (var i = 0; i < 20; i++) {
-            console.log("Adolfo Tweet #" + i + ": " + tweets[i].text);
+    var params = { screen_name: 'juarezadolfo' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < 20; i++) {
+                console.log("Adolfo Tweet #" + i + ": " + tweets[i].text);
+            }
         }
+    });
+    // };
+    
+
+
+
+// 	//SPOTIFY
+// eventually add spotify function
+// function spotifySong(songName) {
+spotify.search({ type: 'track', query: 'The Sign' }, function (err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+    for (var i = 0; i < 5; i++) {
+        console.log("------------------------------------");
+        for (var j = 0; j < data.tracks.items[i].artists.length; j++) {
+            console.log("Artist(s): " + data.tracks.items[i].artists[j].name);
+        }
+        console.log("Song's Name: " + data.tracks.items[i].name);
+        console.log("Preview Link: " + data.tracks.items[i].preview_url);
+        console.log("Album: " + data.tracks.items[i].album.name);
     }
 });
-// };
+//   };
+
+
+
 
 // OMDB Request
 
@@ -61,33 +70,24 @@ var movieName = "";
 // Loop through all the words in the node argument
 // And do a little for-loop magic to handle the inclusion of "+"s
 for (var i = 2; i < nodeArgs.length; i++) {
-
     if (i > 2 && i < nodeArgs.length) {
-
         movieName = movieName + "+" + nodeArgs[i];
-
     }
-
     else {
-
         movieName += nodeArgs[i];
-
     }
 }
 
 // Then run a request to the OMDB API with the movie specified
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
 
 request(queryUrl, function (error, response, body) {
 
     // If the request is successful
     if (!error && response.statusCode === 200) {
 
-        // Parse the body of the site and recover just the imdbRating
-        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+        // Parse the body of the site and recover the Title, imdbRating, etc...
         console.log("Title: " + JSON.parse(body).Title);
         console.log("Release Year: " + JSON.parse(body).Year);
         console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
@@ -99,6 +99,29 @@ request(queryUrl, function (error, response, body) {
         console.log("------------------------------------");
     }
 });
+
+
+
+// DO WHAT IT SAYS
+// function for 'do-what-it-says'
+// This block of code will read from the "random.txt" file.
+// It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+// The code will store the contents of the reading inside the variable "data"
+fs.readFile("random.txt", "utf8", function(error, data) {
+
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+  
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+  
+    // We will then run a spotify search based on this 'random.txt' file dataArr.
+    spotifySong(dataArr[1]);
+  
+  });
+  
 
 
 
