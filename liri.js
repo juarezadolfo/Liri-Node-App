6d04c2a75dc6c5985eda336c31bac7e4445ab92a
juarezadolfo <juarezadolfo@hotmail.com>
2017-12-25 21:35:27 -0800

    fixed twitter results and added spotify info - fixed spotify npm

--- liri.js
+++ liri.js
@@ -5,8 +5,23 @@ var input = process.argv[2];
 var keys = require('./key.js');
 var fs = require('fs');
 var Twitter = require('twitter');
-// var spotify = require('spotify');
+var Spotify = require('node-spotify-api');
 var request = require('request');
+
+// 	//Spotify
+// var spotify = new Spotify({
+//     id: <'1440450cdb184bdd98f1a9909071e9a8'>,
+//     secret: <'c3f55ce679504b6d9a8170716837e5c0'>
+//   });
+   
+//   spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
+   
+//   console.log(data); 
+//   });
+
 
 // 	//TWITTER
 // //if user input is my-tweets this will post the text only from my last 20 tweets
@@ -17,10 +32,15 @@ var client = new Twitter(keys.twitterKeys);
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
         for (var i = 0; i < 20; i++){
-            console.log(tweets[i].text);
+            console.log("Adolfo Tweet #" + i + ": " + tweets[i].text);
         }
     }
   });
+
+
+
+
+  
 
 
 
