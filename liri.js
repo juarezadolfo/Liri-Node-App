d118a7330b41c66e93565e2094eab09bd80e9f2b
juarezadolfo <juarezadolfo@hotmail.com>
2017-12-26 18:24:59 -0800

    final works - added switch case & fixed functions - update readme/bonus features next

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 // Take in the command line arguments
+// Store all of the arguments in an array
 var nodeArgs = process.argv;
 //captures the user input on the command line
 var input = process.argv[2];
@@ -16,14 +17,54 @@ var request = require('request');
 var client = new Twitter(keys.twitterKeys);
 var spotify = new Spotify(keys.spotifyKeys);
 
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s - from exercises
+// change index start to 3 due to input comes after command and command is index 2
+for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+        x = x + "+" + nodeArgs[i];
+    }
+    else {
+        x += nodeArgs[i];
+    }
+}
 
+// switch case
+switch (input) {
+    case "my-tweets":
+        myTweets();
+        break;
 
+    case "spotify-this-song":
+        if (x) {
+            spotifySong(x);
+        } else {
+            spotifySong("Ace of Base - The Sign");
+        }
+        break;
+
+    case "movie-this":
+        if (x) {
+            omdbData(x)
+        } else {
+            omdbData("Mr. Nobody")
+        }
+        break;
+
+    case "do-what-it-says":
+        doIt();
+        break;
+
+    default:
+        console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
+        break;
+}
 
 // 	//TWITTER
 // //if user input is 'my-tweets' 
 // this will post the text only from my last 20 tweets
-// eventually add Twitter function
-// function myTweets() {
+// add Twitter function
+function myTweets() {
     var params = { screen_name: 'juarezadolfo' };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
@@ -32,99 +73,68 @@ var spotify = new Spotify(keys.spotifyKeys);
             }
         }
     });
-    // };
-    
-
-
+};
 
 // 	//SPOTIFY
-// eventually add spotify function
-// function spotifySong(songName) {
-spotify.search({ type: 'track', query: 'The Sign' }, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
-    for (var i = 0; i < 5; i++) {
-        console.log("------------------------------------");
-        for (var j = 0; j < data.tracks.items[i].artists.length; j++) {
-            console.log("Artist(s): " + data.tracks.items[i].artists[j].name);
+// add spotify function
+function spotifySong(song) {
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
         }
-        console.log("Song's Name: " + data.tracks.items[i].name);
-        console.log("Preview Link: " + data.tracks.items[i].preview_url);
-        console.log("Album: " + data.tracks.items[i].album.name);
-    }
-});
-//   };
-
-
-
+        for (var i = 0; i < 5; i++) {
+            console.log("------------------------------------");
+            for (var j = 0; j < data.tracks.items[i].artists.length; j++) {
+                console.log("Artist(s): " + data.tracks.items[i].artists[j].name);
+            }
+            console.log("Song's Name: " + data.tracks.items[i].name);
+            console.log("Preview Link: " + data.tracks.items[i].preview_url);
+            console.log("Album: " + data.tracks.items[i].album.name);
+        }
+    });
+};
 
 // OMDB Request
 
-// Store all of the arguments in an array
-var nodeArgs = process.argv;
-
-// Create an empty variable for holding the movie name
-var movieName = "";
-
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-for (var i = 2; i < nodeArgs.length; i++) {
-    if (i > 2 && i < nodeArgs.length) {
-        movieName = movieName + "+" + nodeArgs[i];
-    }
-    else {
-        movieName += nodeArgs[i];
-    }
-}
-
 // Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+function omdbData(movieName) {
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
 
-request(queryUrl, function (error, response, body) {
+    request(queryUrl, function (error, response, body) {
 
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
 
-        // Parse the body of the site and recover the Title, imdbRating, etc...
-        console.log("Title: " + JSON.parse(body).Title);
-        console.log("Release Year: " + JSON.parse(body).Year);
-        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-        console.log("Country of Origin: " + JSON.parse(body).Country);
-        console.log("Language: " + JSON.parse(body).Language);
-        console.log("Plot: " + JSON.parse(body).Plot);
-        console.log("Actors: " + JSON.parse(body).Actors);
-        console.log("------------------------------------");
-    }
-});
-
-
+            // Parse the body of the site and recover the Title, imdbRating, etc...
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+            console.log("Country of Origin: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("------------------------------------");
+        }
+    });
+};
 
 // DO WHAT IT SAYS
 // function for 'do-what-it-says'
 // This block of code will read from the "random.txt" file.
 // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
 // The code will store the contents of the reading inside the variable "data"
-fs.readFile("random.txt", "utf8", function(error, data) {
 
-    // If the code experiences any errors it will log the error to the console.
-    if (error) {
-      return console.log(error);
-    }
-  
-    // Then split it by commas (to make it more readable)
-    var dataArr = data.split(",");
-  
-    // We will then run a spotify search based on this 'random.txt' file dataArr.
-    spotifySong(dataArr[1]);
-  
-  });
-  
-
-
-
-
-
-
+function doIt() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+        // We will then run a spotify search based on this 'random.txt' file dataArr.
+        spotifySong(dataArr[1]);
+    });
+}
